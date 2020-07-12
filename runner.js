8b272d77d518c570cc156f0771036f0c221a4472
faiza203 <60883181+faiza203@users.noreply.github.com>
2020-07-12 17:25:52 +0500

    Update runner.js

--- runner.js
+++ runner.js
@@ -10,4 +10,31 @@ class Runner {
     this.testFiles = [];
   }
 
+  async runTests() {
+    for (let file of this.testFiles) {
+      console.log(chalk.gray(`---- ${file.shortName}`));
+      const beforeEaches = [];
+      global.render = render;
+      global.beforeEach = fn => {
+        beforeEaches.push(fn);
+      };
+      global.it = async (desc, fn) => {
+        beforeEaches.forEach(func => func());
+        try {
+          await fn();
+          console.log(chalk.green(`\tOK - ${desc}`));
+        } catch (err) {
+          const message = err.message.replace(/\n/g, '\n\t\t');
+          console.log(chalk.red(`\tX - ${desc}`));
+          console.log(chalk.red('\t', message));
+        }
+      };
+
+      try {
+        require(file.name);
+      } catch (err) {
+        console.log(chalk.red(err));
+      }
+    }
+  }
 }
