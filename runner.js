dc17e6242ae81707a3a78dfed72e9ea61c99aa23
faiza203 <60883181+faiza203@users.noreply.github.com>
2020-07-12 17:26:35 +0500

    Update runner.js

--- runner.js
+++ runner.js
@@ -37,4 +37,21 @@ class Runner {
       }
     }
   }
+
+  async collectFiles(targetPath) {
+    const files = await fs.promises.readdir(targetPath);
+
+    for (let file of files) {
+      const filepath = path.join(targetPath, file);
+      const stats = await fs.promises.lstat(filepath);
+
+      if (stats.isFile() && file.includes('.test.js')) {
+        this.testFiles.push({ name: filepath, shortName: file });
+      } else if (stats.isDirectory() && !forbiddenDirs.includes(file)) {
+        const childFiles = await fs.promises.readdir(filepath);
+
+        files.push(...childFiles.map(f => path.join(file, f)));
+      }
+    }
+  }
 }
